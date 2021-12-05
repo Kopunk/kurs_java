22a8448515112703f8de58d6c1a61651db160c5a
Wojtek Kopanski <wilq.xp@gmail.com>
2021-12-05 16:12:08 +0100

    zestaw_9: Arkanoid - od nowa do implementacji przegranej

--- zestaw_9/Arkanoid/Arkanoid.java
+++ zestaw_9/Arkanoid/Arkanoid.java
@@ -1,69 +1,15 @@
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Toolkit;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionListener;
-import java.awt.geom.Ellipse2D;
-import java.awt.geom.Rectangle2D;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
-
-class Belka extends Rectangle2D.Float {
-    Belka(int x) {
-        this.x = x;
-        this.y = 170;
-        this.width = 60;
-        this.height = 10;
-    }
-
-    void setX(int x) {
-        this.x = x;
-    }
-}
-
-class Plansza extends JPanel implements MouseMotionListener {
-    Belka b;
-    Kulka a;
-    SilnikKulki s;
-
-    Kafelka t;
-
-    Plansza() {
-        super();
-        addMouseMotionListener(this);
-
-        b = new Belka(100);
-        a = new Kulka(this, 100, 100, 1, 1);
-        s = new SilnikKulki(a);
-
-        t = new Kafelka(5, 5);
-    }
-
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Graphics2D g2d = (Graphics2D) g;
-
-        g2d.fill(a);
-        g2d.fill(b);
-
-        g2d.fill(t);
-    }
-
-    public void mouseMoved(MouseEvent e) {
-        b.setX(e.getX() - 50);
-        repaint();
-    }
-
-    public void mouseDragged(MouseEvent e) {
-
-    }
-}
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.awt.geom.*;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 class Kulka extends Ellipse2D.Float {
     Plansza p;
     int dx, dy;
+
+    int punkty;
 
     Kulka(Plansza p, int x, int y, int dx, int dy) {
         this.x = x;
@@ -74,6 +20,8 @@ class Kulka extends Ellipse2D.Float {
         this.p = p;
         this.dx = dx;
         this.dy = dy;
+
+        this.punkty = 0;
     }
 
     void nextKrok() {
@@ -82,11 +30,47 @@ class Kulka extends Ellipse2D.Float {
 
         if (getMinX() < 0 || getMaxX() > p.getWidth())
             dx = -dx;
-        if (getMinY() < 0 || getMaxY() > p.getHeight())
+        if (getMinY() < 0)
             dy = -dy;
+        else if (getMaxY() > p.getHeight()) {
+            dy = -dy;
+            // TODO add gameover
+            // p.tekstKoncowy = "GAME OVER\nzdobyto " + punkty + " pkt.";
+        }
+
+        // odbicie od belki
+        odbicie(p.b);
+
+        // odbicie od cegielek
+        for (Cegielka[] cegielka1 : p.c) {
+            for (Cegielka cegielka : cegielka1) {
+                System.out.println(cegielka);
+                if (!(cegielka.zbita)) {
+                    if (odbicie(cegielka)) {
+                        cegielka.setZbita(true);
+                        punkty++;
+                    }
+                }
+            }
+        }
 
         p.repaint();
         Toolkit.getDefaultToolkit().sync();
+
+    }
+
+    boolean odbicie(Rectangle2D.Float obiekt) {
+        if (this.intersects(obiekt)) {
+            if (getMaxY() >= obiekt.getMinY()
+                    && (getMinX() + this.width / 2 >= obiekt.getMinX()
+                            && getMaxX() - this.width / 2 <= obiekt.getMaxX())) {
+                dy = -dy;
+            } else {
+                dx = -dx;
+            }
+            return true;
+        }
+        return false;
     }
 }
 
@@ -109,18 +93,110 @@ class SilnikKulki extends Thread {
     }
 }
 
-class Kafelka extends Rectangle2D.Float {
-    Kafelka(int x, int y) {
+class Belka extends Rectangle2D.Float {
+    Belka(int x) {
+        this.x = x;
+        this.y = 270;
+        this.width = 60;
+        this.height = 10;
+    }
+
+    void setX(int x) {
+        this.x = x;
+    }
+}
+
+class Cegielka extends Rectangle2D.Float {
+    boolean zbita;
+
+    Cegielka(int x, int y) {
         this.x = x;
         this.y = y;
-        this.width = 30;
+        this.width = 55;
         this.height = 10;
+        this.zbita = false;
+    }
+
+    void setZbita(boolean zbita) {
+        this.zbita = zbita;
+    }
+
+    @Override
+    public String toString() {
+        return new String("cegielka w " + x + " : " + y + "\t" + zbita);
+    }
+}
+
+class Plansza extends JPanel implements MouseMotionListener {
+    Belka b;
+    Kulka a;
+    SilnikKulki s;
+    Cegielka[][] c = {
+            { null, null, null, null, null, null },
+            { null, null, null, null, null, null },
+            { null, null, null, null, null, null },
+            { null, null, null, null, null, null },
+            { null, null, null, null, null, null }
+    };
+    String tekstKoncowy;
+
+    Plansza() {
+        super();
+        addMouseMotionListener(this);
+
+        b = new Belka(100);
+        a = new Kulka(this, 100, 250, 1, -1);
+        s = new SilnikKulki(a);
+
+        int y = 10;
+        for (int i = 0; i < 5; i++) {
+            int x = 10;
+            for (int j = 0; j < 6; j++) {
+                c[i][j] = new Cegielka(x, y);
+                x += 65;
+            }
+            y += 20;
+        }
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+
+        g2d.fill(a);
+        g2d.fill(b);
+
+        for (Cegielka[] cegielka1 : c) {
+            for (Cegielka cegielka : cegielka1) {
+                if (!cegielka.zbita)
+                    g2d.fill(cegielka);
+            }
+        }
+
+        // if (tekstKoncowy != null) {
+        // g2d.drawString(tekstKoncowy, 190, 160);
+        // try {
+        // TimeUnit.SECONDS.sleep(5);
+        // } catch (InterruptedException e) {
+        // System.exit(1);
+        // }
+        // System.exit(0);
+        // }
+    }
+
+    public void mouseMoved(MouseEvent e) {
+        b.setX(e.getX() - 50);
+        repaint();
+    }
+
+    public void mouseDragged(MouseEvent e) {
+
     }
 }
 
 public class Arkanoid {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 Plansza p;
                 p = new Plansza();
